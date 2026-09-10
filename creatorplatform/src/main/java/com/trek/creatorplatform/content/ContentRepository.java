f93ad8955b1446ab5a8977a8bf543c51334aca4a
Trek17 <tanutriambak@gmail.com>
2026-09-10 15:25:35 +0530

    Add Content repository

---
+++ creatorplatform/src/main/java/com/trek/creatorplatform/content/ContentRepository.java
@@ -0,0 +1,6 @@
+package com.trek.creatorplatform.content;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContentRepository extends CrudRepository<Content,Long> {
+}
