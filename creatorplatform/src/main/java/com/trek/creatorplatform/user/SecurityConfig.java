5cd45c36c73168deed2dc345e04afce1f5493ebe
Trek17 <tanutriambak@gmail.com>
2026-09-10 12:19:08 +0530

    Add SecurityFilterChain
    1. After adding spring security, got 401 - authentication error when doing POST
    2. Spring security disabled for now using SecurityFilterChain
    3. Error changed to 403 - refusing request. SpringSecurity by default CSRF enabled
    4. Disabled CSRF for current state. Will implement later when adding security features to project

--- creatorplatform/src/main/java/com/trek/creatorplatform/user/SecurityConfig.java
+++ creatorplatform/src/main/java/com/trek/creatorplatform/user/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.trek.creatorplatform.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,15 @@ public class SecurityConfig {
     public PasswordEncoder encoder()
     {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain web(HttpSecurity http) throws Exception {
+        http.csrf((csrf) -> csrf.disable())
+        .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/users/**").permitAll()
+                );
+
+        return http.build();
     }
 }
